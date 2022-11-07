709f33e2614ad774a9d6d2406152413b930b289c
Aminesam1092 <doublaliamin13@gmail.com>
2022-11-07 23:43:31 +0900

    chat

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php 
+//データベースに接続する//
+try{
+    $pdo=new PDO('mysql:host=localhost;dbname=chat','root','root');
+    $sql="SELECT * FROM `chat`;";
+    $chatarray=$pdo->query($sql);
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +25,23 @@
     <title>Document</title>
 </head>
 <body>
-    <?php echo "あいうえｐ"; ?>
+    <div>
+        <div>
+            <p>17</p>
+            <p>名前</p>
+            <p>2022-10-01 04:13:11</p>
+        </div>
+        <p>本文</p>
+    </div>
+    <form action="" method="post">
+        <div>
+            <input type="submit" value="書き込む" name="submitbutton">
+            <label for="">名前:</label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <textarea name="comment" id="" cols="30" rows="10"></textarea>
+        </div>
+    </form>
 </body>
 </html>
